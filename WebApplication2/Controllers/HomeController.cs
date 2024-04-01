c791b64d97cd135670b18212c1a6f4560431cfba
osama <halasocialacount@gmail.com>
2024-04-02 01:27:10 +0300

    login page

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -39,6 +39,10 @@ namespace WebApplication2.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
