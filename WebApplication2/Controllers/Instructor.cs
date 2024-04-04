4161a06495c8baaa9730bc44350cfcf849a10442
abdullah <abdullah@E-LENOVO-LAPTOP>
2024-04-05 02:20:26 +0300

    injecting the db into the istructor controller

--- WebApplication2/Controllers/Instructor.cs
+++ WebApplication2/Controllers/Instructor.cs
@@ -1,9 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class Instructor : Controller
     {
+        LconsultDBContext _context;
+        public Instructor(LconsultDBContext context) {
+            _context = context;
+
+        }
+
+        public IActionResult test()
+        {
+           string provider= _context.Courses.FirstOrDefault().Platform?.ToString()??"NA";
+            return Content(provider);
+        }
+
         public IActionResult Index()
         {
             return View();
