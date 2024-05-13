2d1e827f7fb73e69d2f8d525e0eb5db59dabf3c4
abdullah <abdullah@E-LENOVO-LAPTOP>
2024-05-13 23:36:19 +0300

    trying to orgnazie controllers

--- WebApplication2/Controllers/CourseController.cs
+++ WebApplication2/Controllers/CourseController.cs
@@ -1,16 +1,100 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+using WebApplication2.Models.Repository;
+using WebApplication2.ViewModels;
 
 namespace WebApplication2.Controllers
 {
     public class CourseController : Controller
     {
         // GET: CourseController
+        private readonly LconsultDBContext _context;
+        private readonly IWebHostEnvironment _environment;
+        private readonly ICourseRepository _courseRepository;
+        public CourseController( LconsultDBContext context, IWebHostEnvironment environment,ICourseRepository Course)
+        {
+            _context = context;
+            _courseRepository = Course;
+            _environment = environment;
+        }
         public ActionResult Index()
         {
             return View();
         }
 
+
+
+        public async Task<IActionResult> GetSubcategories(int categoryId)
+        {
+            var subcategories = await _context.SubCategories
+                .Where(s => s.CategoryId == categoryId)
+                .Select(s => new { s.SubId, s.SubName })
+                .ToListAsync();
+
+            return Json(subcategories);
+        }
+        [HttpGet]
+        public IActionResult AddCourse()
+        {
+            ViewBag.Categories = _context.Categories;
+            ViewBag.Languages = _context.Languages;
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult AddCourse(AddCourseViewModel model)
+        {
+            string uniqeFileName = ProcessUploadFile(model);
+            if (ModelState.IsValid)
+            {
+                Course newCourse = new()
+                {
+                    Title = model.Title,
+                    CategoryId = model.CategoryId,
+                    AddingDate = model.AddingDate,
+                    AverageRating = model.AverageRating,
+                    LastUpdate = model.LastUpdate,
+                    CourseDuration = model.CourseDuration,
+                    CourseDescription = model.CourseDescription,
+                    LanguageId = model.LanguageId,
+                    Level = model.Level,
+                    PriceStatus = model.PriceStatus,
+                    SubcategoryId = model.SubcategoryId,
+                    TopicsCovered = model.TopicsCovered,
+                    Link = model.Link,
+                    VedioLenght = model.VedioLength,
+                    Picture = uniqeFileName,
+                };
+                _courseRepository.Add(newCourse);
+            }
+            return RedirectToAction("AddCourse");
+        }
+
+        private string ProcessUploadFile(AddCourseViewModel model)
+        {
+            string uniqeFileName = null;
+            if (model.Picture is not null)
+            {
+                string uploadFolder = Path.Combine(_environment.WebRootPath, "images/users");
+                uniqeFileName = Guid.NewGuid().ToString() + "_" + model.Picture.FileName;
+                string filePath = Path.Combine(uploadFolder, uniqeFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Picture.CopyTo(fileStream);
+                }
+
+            }
+
+            return uniqeFileName;
+        }
+        public IActionResult InputPartial()
+        {
+            return PartialView("_topicsInputPartialView");
+        }
         // GET: CourseController/Details/5
         public ActionResult Details(int id)
         {
