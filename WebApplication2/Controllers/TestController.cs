84f6b6a3961ba6878c10183dfb75773d8068a8ba
abdullah <abdullah@E-LENOVO-LAPTOP>
2024-05-30 02:42:57 +0300

    configuring the details view in category,

---
+++ WebApplication2/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
