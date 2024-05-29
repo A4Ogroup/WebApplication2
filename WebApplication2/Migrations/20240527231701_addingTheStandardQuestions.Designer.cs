27e91a58ac430d56d6a71e3174b3ee3a8551f69a
abdullah <abdullah@E-LENOVO-LAPTOP>
2024-05-29 20:41:32 +0300

    ...

--- WebApplication2/Migrations/20240527231701_addingTheStandardQuestions.Designer.cs
+++ WebApplication2/Migrations/20240527231701_addingTheStandardQuestions.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication2.Models;
 
@@ -11,9 +12,10 @@ using WebApplication2.Models;
 namespace WebApplication2.Migrations
 {
     [DbContext(typeof(LconsultDBContext))]
-    partial class LconsultDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240527231701_addingTheStandardQuestions")]
+    partial class addingTheStandardQuestions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
