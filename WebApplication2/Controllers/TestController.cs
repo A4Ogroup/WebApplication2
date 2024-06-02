526691e732a892848e2b1ae961e5369412d0d7a7
osama <halasocialacount@gmail.com>
2024-06-02 22:27:45 +0300

    adding new page in test controller with filter feature

--- WebApplication2/Controllers/TestController.cs
+++ WebApplication2/Controllers/TestController.cs
@@ -1,12 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Helpers.Enums;
+using WebApplication2.Models;
+using WebApplication2.ViewModels;
 
 namespace WebApplication2.Controllers
 {
     public class TestController : Controller
     {
+        LconsultDBContext _context;
+
+        public TestController( LconsultDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Test()
         {
             return View();
+        }
+        public IActionResult New(CourseFilterViewModel filter )
+        {
+
+            var courses = _context.Courses.AsQueryable();
+
+
+
+            if (filter.CategoryIds != null && filter.CategoryIds.Any())
+            {
+                courses = courses.Where(c => filter.CategoryIds.Contains(c.CategoryId ?? 0));
+            }
+
+            if (filter.LanguageIds != null && filter.LanguageIds.Any())
+            {
+                courses = courses.Where(c => filter.LanguageIds.Contains(c.LanguageId ?? 0));
+            }
+
+            if (filter.Levels != null && filter.Levels.Any())
+            {
+                courses = courses.Where(c => filter.Levels.Contains(c.Level ?? Level.Begginer));
+            }
+
+            if (filter.VideoLengths != null && filter.VideoLengths.Any())
+            {
+                courses = courses.Where(c => filter.VideoLengths.Contains(c.VedioLength ?? VideoLengthCategory.Short));
+            }
+
+            filter.Courses = courses.ToList();
+
+            // Load data for dropdowns and checkboxes
+            ViewBag.Categories = _context.Courses.Select(c => c.CategoryId).Distinct().ToList();
+            ViewBag.Languages = _context.Courses.Select(c => c.LanguageId).Distinct().ToList();
+
+            return View(filter);
         }
     }
 }
---
+++ WebApplication2/ViewModels/CourseFilterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using WebApplication2.Helpers.Enums;
+using WebApplication2.Models;
+using System.Collections.Generic;
+
+namespace WebApplication2.ViewModels
+{
+    public class CourseFilterViewModel:AddCourseViewModel
+    {
+        public List<int> Ratings { get; set; }
+        public List<byte> CategoryIds { get; set; }
+        public List<int> LanguageIds { get; set; }
+        public List<Level> Levels { get; set; }
+        public List<VideoLengthCategory> VideoLengths { get; set; }
+
+        public List<Course> Courses { get; set; }
+    }
+}
+
