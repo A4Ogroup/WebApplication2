d336b5c1d2a247d79d9cb1e77ccec794c248ab85
osama <halasocialacount@gmail.com>
2024-06-03 18:49:21 +0300

    test new page for filtering

---
+++ WebApplication2/ViewModels/SearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication2.Helpers.Enums;
+using WebApplication2.Models;
+namespace WebApplication2.ViewModels
+{
+    public class SearchViewModel
+    {
+        public SearchViewModel() { }
+
+       public IEnumerable<Course> courses {  get; set; }= new List<Course>();
+       public IEnumerable<Category> categories { get; set; } = new List<Category>();
+       public IEnumerable<Language> languages { get; set; }=new List<Language>();
+       public IEnumerable<Level> levels { get; set; } = new List<Level>();
+    }
+}
