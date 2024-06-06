8711b68393a564c5e213d88af4685aa6b9a933f5
abdullah <abdullah@E-LENOVO-LAPTOP>
2024-06-06 20:35:15 +0300

    configuring course Filtering View in test Controller

--- WebApplication2/ViewModels/CourseFilterViewModel.cs
+++ WebApplication2/ViewModels/CourseFilterViewModel.cs
@@ -4,15 +4,15 @@ using System.Collections.Generic;
 
 namespace WebApplication2.ViewModels
 {
-    public class CourseFilterViewModel:AddCourseViewModel
+    public class CourseFilterViewModel
     {
-        public List<int> Ratings { get; set; }
-        public List<byte> CategoryIds { get; set; }
-        public List<int> LanguageIds { get; set; }
-        public List<Level> Levels { get; set; }
-        public List<VideoLengthCategory> VideoLengths { get; set; }
-
-        public List<Course> Courses { get; set; }
+        public List<double>? Ratings { get; set; }
+        public List<byte>? CategoryIds { get; set; }
+        public List<int>? LanguageIds { get; set; }
+        public List<Level>? Levels { get; set; }
+        public List<VideoLengthCategory>? VideoLengths { get; set; }
+        public List<bool>? IsFree { get; set; }
+        public List<Course>? Courses { get; set; }
     }
 }
 
