fbe653cf779af5b9255a49ec1884216b5e3c6447
abdullah <abdullah@E-LENOVO-LAPTOP>
2024-07-21 23:02:02 +0300

    adding namespaces

--- WebApplication2/Controllers/CourseController.cs
+++ WebApplication2/Controllers/CourseController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Helpers;
 using WebApplication2.Helpers.Enums;
