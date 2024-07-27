868dc038fcb2531bb589b597e08f9348b030a207
abdullah <abdullah@E-LENOVO-LAPTOP>
2024-07-27 17:11:57 +0300

    adding the image methodology

--- WebApplication2/Controllers/Student.cs
+++ WebApplication2/Controllers/Student.cs
@@ -12,13 +12,17 @@ namespace WebApplication2.Controllers
     public class StudentController : Controller
     {
         private readonly LconsultDBContext _context;
+        private readonly IWebHostEnvironment _environment;
+
         private readonly ICourseRepository _courseRepository;
         private readonly IReviewRepository _reviewRepository;
         private readonly IStudentRepository _studentRepository; 
         private readonly UserManager<User> _userManager;
 
-        public StudentController(LconsultDBContext context , IReviewRepository reviewRepository,ICourseRepository courseRepository,IStudentRepository studentRepository, UserManager<User> userManager)
+        public StudentController(LconsultDBContext context , IReviewRepository reviewRepository,ICourseRepository courseRepository,IStudentRepository studentRepository,IWebHostEnvironment environment
+            , UserManager<User> userManager)
         {
+            _environment = environment;
             _context = context;
             _reviewRepository = reviewRepository;
             _courseRepository = courseRepository;
@@ -78,6 +82,7 @@ namespace WebApplication2.Controllers
                     FirstName = student.StudentNavigation.FirstName,
                     LastName = student.StudentNavigation.LastName,
                     Gender = student.StudentNavigation.Gender,
+                    ExistingPhotoPath=student.StudentNavigation.Picture
                 };
                 return View(_student);
             }
@@ -95,7 +100,17 @@ namespace WebApplication2.Controllers
             _student.StudentNavigation.FirstName = model.FirstName;
             _student.StudentNavigation.LastName = model.LastName;
             _student.StudentNavigation.Gender = model.Gender;
+            if (model.Picture != null)
+            {
 
+                if (model.ExistingPhotoPath != null)
+                {
+                    string filePath = Path.Combine(_environment.WebRootPath, "images/users", model.ExistingPhotoPath);
+                    System.IO.File.Delete(filePath);
+                }
+                _student.StudentNavigation.Picture = ProcessUploadFile(model, x => x.Picture);
+
+            }
             _studentRepository.Update(_student);
             _studentRepository.Save();
             TempData["Success"] = "Student data edited successfully!";
@@ -108,7 +123,23 @@ namespace WebApplication2.Controllers
             return View();
         }
 
+        private string ProcessUploadFile<T>(T model, Func<T, IFormFile> pictureAccessor)
+        {
+            string uniqeFileName = null;
+            var picture = pictureAccessor(model);
+            if (picture is not null)
+            {
+                string uploadFolder = Path.Combine(_environment.WebRootPath, "images/users");
+                uniqeFileName = Guid.NewGuid().ToString() + "_" + picture.FileName;
+                string filePath = Path.Combine(uploadFolder, uniqeFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    picture.CopyTo(fileStream);
+                }
+            }
 
+            return uniqeFileName;
+        }
 
 
     }
